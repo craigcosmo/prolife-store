f03d4dec3aa99a3f7492af8965854ada25abfa3f
Craig Nguyen <craigcosmo@gmail.com>
2019-06-22 14:02:23 +0700

    update

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -59,53 +59,6 @@ const css = {
 		paddingTop:'75px',
 		paddingBottom:'75px',
 	},
-	button : {
-		background: '#333333',
-		width: '100%',
-		height: '50px',
-		lineHeight:'50px',
-		color: '#fff',
-		textTransform:'uppercase',
-	},
-	area: {
-		width:'100%',
-		height:'237px',
-		fontSize: '18px',
-		paddingLeft:'20px',
-		paddingTop:'20px',
-		marginBottom:'43px',
-	},
-	border :{
-		border: '1px solid #C9C9C9',
-
-	},
-	inwrap:{
-		display:'flex',
-		marginBottom:'43px',
-	},
-	input : {
-		flex:1,
-		height:'68px',
-		paddingLeft:'20px',
-		fontSize: '18px',
-		border: '1px solid #C9C9C9',
-	},
-	first:{
-		marginRight:'43px',
-	},
-	sendBtn : {
-		backgroundColor:'#333333',
-		color:'#fff',
-		fontSize:'25px',
-		fontWeight:300,
-		height:'68px',
-		lineHeight:'68px',
-		width:'100%',
-	},
-	boxwrap: {
-		display:'flex',
-		margin:'auto',
-	}
 }
 
 
